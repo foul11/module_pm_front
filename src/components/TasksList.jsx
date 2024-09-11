4331f726a34cf8cf2f83698d4b485aa55bc324d7
foul11 <admin@arturka.net>
2024-09-11 19:57:01 +0700

    add descriptions

--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -6,7 +6,7 @@ import { complete, del } from '../store/tasks';
 import { editTask } from '../store/popup';
 
 import { Box } from '@mui/system';
-import { Checkbox, IconButton, List, ListItem } from '@mui/material';
+import { Checkbox, Divider, IconButton, List, ListItem, Paper, Typography } from '@mui/material';
 
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
@@ -24,31 +24,55 @@ export default function TasksList({ filter }) {
     
     return <>
         <List>
-            {filteredTasks.map((task) => (
-                <ListItem
-                    key={task.id}
-                    sx={{ gap: 1 }}
-                >
-                    <Checkbox
-                        checked={task.complete}
-                        onChange={(e) => dispatch(complete({ id: task.id, complete: e.target.checked }))}
-                    />
+            {filteredTasks.map((task, i) => (
+                <ListItem key={task.id} sx={{ flexDirection: 'column', py: 0 }}>
+                    <Box sx={{
+                        width: '100%',
+                        display: 'flex',
+                        alignItems: 'center',
+                        gap: 1,
+                        pt: 1,
+                    }}>
+                        <Checkbox
+                            checked={task.complete}
+                            onChange={(e) => dispatch(complete({ id: task.id, complete: e.target.checked }))}
+                        />
+                        
+                        {task.name}
+                        
+                        <Box sx={{ flexGrow: 1 }} />
+                        
+                        <IconButton
+                            onClick={() => dispatch(editTask({ id: task.id }))}
+                        >
+                            <EditIcon />
+                        </IconButton>
+                        
+                        <IconButton
+                            onClick={() => dispatch(del({ id: task.id }))}
+                        >
+                            <DeleteIcon />
+                        </IconButton>
+                    </Box>
                     
-                    {task.name}
-                    
-                    <Box sx={{ flexGrow: 1 }} />
-                    
-                    <IconButton
-                        onClick={() => dispatch(editTask({ id: task.id }))}
+                    {task.desc && <Box sx={{
+                            alignSelf: 'flex-start',
+                            whiteSpace: 'pre-wrap',
+                            width: '100%',
+                            pl: 6,
+                            pb: 2,
+                            pr: 12,
+                            flexGrow: 1,
+                        }}
                     >
-                        <EditIcon />
-                    </IconButton>
+                        <Paper elevation={2} sx={{ p: 1 }}>
+                            <Typography>
+                                {task.desc}
+                            </Typography>
+                        </Paper>
+                    </Box> || <></>}
                     
-                    <IconButton
-                        onClick={() => dispatch(del({ id: task.id }))}
-                    >
-                        <DeleteIcon />
-                    </IconButton>
+                    {filteredTasks.length !== i + 1 && <Divider sx={{ width: '100%' }} />}
                 </ListItem>
             ))}
             
